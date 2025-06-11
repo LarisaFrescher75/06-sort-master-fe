4f6c8fc3c5ae06dada2c9eb4511d4430dc29bc9c
LarisaFrescher75 <stas.frescher@gmail.com>
2025-06-11 17:45:54 +0200

    Добавлено описание страницы About

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,5 +1,68 @@
-
-
 export default function About() {
-  return <div>About Page</div>;
+  return (
+    <div className="flex flex-col md:flex-row gap-6 p-4">
+      <section className="group flex-1 rounded-2xl overflow-hidden shadow-lg transition-transform duration-300 hover:scale-105">
+  <img
+    src="https://chisto-moy.ru/images/ochistka-musornyh-kontejnerov-mojkoj-vysokogo-davleniya.jpg"
+    className="w-full h-64 md:h-96 object-cover"
+    alt="recycling"
+  />
+  <div className="p-6 text-fuchsia-600 bg-white">
+    <p className="text-lg indent-5">
+            Separate garbage collection, As for household garbage, there are
+            several colorful containers near every house in Germany. How do you
+            define the "palette" of waste? The blue basket is for paper and
+            cardboard, the blue tanks are the easiest to deal with. So, you can
+            throw it out there: magazines;newspapers;flyers;egg
+            cartons;corrugated cardboard Green and white urns In these
+            containers should be discarded glass. The white basket is for white
+            glass, and the green basket is for colored glass. If you don't have
+            these containers near your house, look for trash cans labeled
+            Glasigus in your area. There are even special programs that allow
+            you to find such containers on the map. Yellow and orange container
+            The yellow or orange basket is designed for metal and plastic. They
+            can be thrown into such bins.:Metal;plastic packaging;non-returnable
+            plastic bottles;condiment bottles;yogurt containers;non-returnable
+            cans;metal cans;composite packaging. Special waste Used electronics,
+            batteries, neon lights and energy-saving lamps are not thrown into
+            the trash. Take them to special locations so that they can be
+            properly recycled. The list of reception points for equipment, light
+            bulbs and batteries includes:Deutsche Post — you print the label and
+            send the used batteries and electronics for free.MediaMarkt — refund
+            is possible at any branch.DM, Aldi and REWE are trash cans near cash
+            registers in some branches.Batteries for cars and motorcycles have a
+            deposit (Pfand) of 7.50 euros. If you return them, you will receive
+            this small financial bonus.
+          </p>
+        </div>
+      </section>
+
+      <section className="group flex-1 rounded-2xl overflow-hidden shadow-lg transition-transform duration-300 hover:scale-105">
+  <img
+    src="https://avatars.dzeninfra.ru/get-zen_doc/3517023/pub_62d02f157024e56b3b80c72f_62d03a52ac1f664e51f3a0dd/scale_1200"
+    className="w-full h-64 md:h-96 object-cover"
+    alt="recycling"
+  />
+  <div className="p-6 text-#7029c0 bg-#4adc60-800">
+    <p className="text-lg indent-5">
+      The rest, or remaining garbage (Restmull) is collected in 
+      black tanks, which are either in the basement of the house
+       or near the house and are taken out about once a month. Recycling of such 
+       garbage is quite expensive, on average, you have to pay from 10 to 25 euros per
+        month for garbage collection. And if a family, for example, needs to take out
+         this tank more often, then it costs even more. Floppy disks, empty lighters, 
+         photos, rubber, personal care products, just trash, leather, colored napkins,
+       vacuum cleaner bags, kitchen rags, cigarette butts are thrown into this tank.
+      Perhaps in any other country, such a system would not really take root.
+      
+      But you can't scare the Germans with a couple of extra rules. Therefore,
+      they can really be called the world champions in waste disposal and recycling.
+      Needless to say, one can only envy such a national consciousness.
+      After all, every resident of Germany works every day for the benefit of the environment.
+    </p>
+  </div>
+</section>
+
+    </div>
+  );
 }
